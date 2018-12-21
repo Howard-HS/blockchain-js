fb30b425844e26512ab83b61d9e926d5ecdf3633
Howard Chang <hschang.2008@gmail.com>
2018-12-21 08:22:29 +0800

    WIP: Difficulty

---
+++ src/Difficulty.js
@@ -0,0 +1,17 @@
+function computeDifficulty(size) {
+  
+}
+
+
+
+class Difficulty {
+  constructor(size) {
+    this.size = size
+  }
+
+  getDifficulty() {
+    return   
+  }
+}
+
+console.log(startingDifficulty)
